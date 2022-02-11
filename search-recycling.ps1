0006fbd71e2b0d817e7f24c9d08c929326d8ed0e
Andrea Goodson <akgdev@icloud.com>
2022-02-11 14:43:04 +0000

    Update search-recycling.ps1

--- search-recycling.ps1
+++ search-recycling.ps1
@@ -1,2 +1,2 @@
 Connect-PnPOnline -Url "https://[your_tenant].sharepoint.com/sites/[your_site]" -Interactive -ForceAuthentication
-Get-PnPRecycleBinItem -RowLimit 10000 | Where-Object{($_.DeletedDate -gt "1/30/2022") -and ($_.Title -match "Engineering")}
+Get-PnPRecycleBinItem -RowLimit 10000 | Where-Object{($_.DeletedDate -gt "1/30/2022") -and ($_.DeletedByEmail -eq "person@example.com") -and ($_.Title -match "Engineering")}
